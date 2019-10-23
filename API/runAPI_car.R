817094425ebf4311ac8cb3774e32a2d3831b786f
kibiras <kdaugela@gmail.com>
2019-10-23 11:49:55 +0300

    update

--- API/runAPI_car.R
+++ API/runAPI_car.R
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 options(encoding='UTF-8')
 library(plumber)
 r <- plumb("bs.R")
 r$run(host = "0.0.0.0", port = 8000, swagger=TRUE)
-=======
-options(encoding='UTF-8')
-library(plumber)
-r <- plumb("bs.R")
-r$run(host = "0.0.0.0", port = 8000)
->>>>>>> 39e5ea8b74ac7e7553edd3f3330744f85e1b9677
