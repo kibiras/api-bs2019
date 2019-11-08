201f5811ad52931cdd367662d6e126bca56291e5
Petrikonis <S87667@corp1.ad1.seb.net>
2019-11-08 10:31:48 +0200

    Improve registration

--- build-stuff-gui/src/app/components/startpage/startpage.component.ts
+++ build-stuff-gui/src/app/components/startpage/startpage.component.ts
@@ -1,8 +1,10 @@
 import { RegistrationService } from './../../services/registration.service';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { faEye, faFlagCheckered, faQuestion, faSlidersH, faFastBackward } from '@fortawesome/free-solid-svg-icons';
 import { GameService } from './../../services/game.service';
+
+const RFC2822_EMAIL_REGEX = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@" + '(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?', 'i');
 
 @Component({
   selector: 'app-startpage',
@@ -16,7 +18,7 @@ export class StartpageComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private gameService: GameService, private registrationService: RegistrationService) {
     this.modalForm = this.formBuilder.group({
       name: '',
-      email: '',
+      email: ['', Validators.pattern(RFC2822_EMAIL_REGEX)],
       agreeToShowInLeaderBoard: false,
       agreeToGetInformation: false
     });
@@ -27,6 +29,37 @@ export class StartpageComponent implements OnInit {
   faQuestion = faQuestion;
   faFlagCheckered = faFlagCheckered;
   faFastBackward = faFastBackward;
+
+  isInvalid(): boolean {
+    const input = this.modalForm.controls.email;
+    if (input) {
+      let isInvalid = input.invalid && ((input.dirty && (!input.errors || !input.errors.required)) || input.touched);
+      if (input.value) {
+        this.changeCheckBoxes(isInvalid);
+      } else {
+        this.changeCheckBoxes(true);
+      }
+      return isInvalid;
+    } else {
+      this.changeCheckBoxes(false);
+      return false;
+    }
+  }
+  isInvalidFormat(): boolean {
+    let isInvalidFormat = this.modalForm.controls.email.errors.pattern;
+    this.changeCheckBoxes(isInvalidFormat);
+    return isInvalidFormat;
+  }
+
+  uncheck() {
+    if (this.modalForm.controls.agreeToGetInformation.value) {
+      this.modalForm.controls.email.patchValue('');
+    }
+  }
+
+  private changeCheckBoxes(isInvalid: boolean) {
+    this.modalForm.controls.agreeToGetInformation.patchValue(!isInvalid);
+  }
 
   public startJustDrive() {
     this.gameService.startJustDrive().subscribe(
@@ -55,6 +88,9 @@ export class StartpageComponent implements OnInit {
   public startGame() {
     this.ongoingRegistration = true;
     localStorage.setItem('driveName', this.modalForm.controls.name.value);
+    if (!this.modalForm.controls.agreeToGetInformation.value) {
+      this.modalForm.controls.email.patchValue('');
+    }
     localStorage.setItem('driveEmail', this.modalForm.controls.email.value);
     localStorage.setItem('agreeToShowInLeaderBoard', this.modalForm.controls.agreeToShowInLeaderBoard.value);
     localStorage.setItem('agreeToGetInformation', this.modalForm.controls.agreeToGetInformation.value);
